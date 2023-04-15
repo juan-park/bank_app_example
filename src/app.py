f2039fbd8eef1f265c1dca4a5f7459e1276b0240
JuAn Park <gomtang@JuAns-MacBook-Pro.local>
2023-04-15 08:20:23 -0400

    basic gui

--- src/app.py
+++ src/app.py
@@ -1,19 +1,37 @@
-import tkinter as tk
-from tkinter import ttk
-from windows.login import LogIn
+import tkinter as tk  
+from tkinter import font as tkfont
+from .windows import LogIn, Dashboard, AccountBalance
+
 
 class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
 
-        self.geometry('300x200')
-        self.title('Main Window')
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        # place a button on the root window
-        ttk.Button(self,
-                text='Open a window',
-                command=self.open_window).pack(expand=True)
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-    def open_window(self):
-        window = LogIn(self)
-        window.grab_set()
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (LogIn, Dashboard, AccountBalance):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("LogIn")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
